187c9757fd5b412f884448fb9a55d1721a2be0dc
Shashwat Joshi <shashwat2002joshi@gmail.com>
2020-02-17 20:46:01 +0530

    Add files via upload

---
+++ Shutdown.py
@@ -0,0 +1,17 @@
+import os
+from time import sleep
+
+
+def shutdown():
+    ans = input("Do you Love me ? Y/N \n")
+    if ans == 'Y' or ans == 'y':
+        print("I love you to !!")
+        sleep(2)
+    else:
+        print("That's Rude !")
+        sleep(1)
+        os.system("shutdown /s /t 30")          #("shutdown /s /t 30")
+                                                #(/s) -> to shutdown, replace with /r to restart
+                                                #(/t) -> represents time
+                                                #(30) -> time in seconds
+shutdown()

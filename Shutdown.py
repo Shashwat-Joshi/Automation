09db78037d31fd589ae755d681939dcc8521e48d
Shashwat Joshi <shashwat2002joshi@gmail.com>
2020-05-09 01:40:38 +0530

    Update Shutdown.py

--- Shutdown.py
+++ Shutdown.py
@@ -3,14 +3,14 @@ from time import sleep
 
 
 def shutdown():
-    ans = input("Do you Love me ? Y/N \n")
+    ans = input("Shutdown System ? Y/N \n")
     if ans == 'Y' or ans == 'y':
-        print("I love you to !!")
+        print("System will shut down in 30 sec !!")
         sleep(2)
-    else:
-        print("That's Rude !")
-        sleep(1)
-        os.system("shutdown /s /t 30")          #("shutdown /s /t 30")
-                                                #(/s) -> to shutdown, replace with /r to restart
-shutdown()                                      #(/t) -> represents time
-                                                #(30) -> time in seconds
+        os.system("shutdown /s /t 30")                       #("shutdown /s /t 30")
+    else:                                                    #(/s) -> to shutdown, replace with /r to restart
+        print("BYE BYE !")                                   #(/t) -> represents time
+        sleep(1)                                             #(30) -> time in seconds
+                                                                                 
+shutdown()                                     
+                                                
